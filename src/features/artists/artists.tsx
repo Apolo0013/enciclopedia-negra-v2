1d55f905e87aaeda2e7737e7829552e6513c2583
Apolo0013 <148099939+Apolo0013@users.noreply.github.com>
2026-08-11 19:48:12 -0300

    Responsividade  no Footer

--- src/features/artists/artists.tsx
+++ src/features/artists/artists.tsx
@@ -13,7 +13,7 @@ function Artists() {
             <ArtistsDescription/>
             <ArtistsGrid />
             <FeaturesSection />
-            <Footer />
+            <Footer/>
         </main>
     )
 }
