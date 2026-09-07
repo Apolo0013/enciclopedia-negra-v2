c48e23d30b2209b2fc815a4aeac27bbaf6675b90
Apolo0013 <148099939+Apolo0013@users.noreply.github.com>
2026-09-07 03:23:25 -0300

    responsavidade button-view, botao responsavel por expandir o conteudo do artista

--- src/features/home/components/contentHome/contentHome.tsx
+++ src/features/home/components/contentHome/contentHome.tsx
@@ -1,12 +1,21 @@
+//hook
+import { useNavigate } from 'react-router-dom'
+//css
 import './ContentHome.scss'
+//base URL
+const baseURL = import.meta.env.BASE_URL
 
 function ContentHome() {
+    const nv = useNavigate()
     return (
         <section className='content-home'>
             <h1>ENCICLOPÉDIA NEGRA</h1>
             <p>Um espaço para descobrir artistas negros de A a Z.
                 Histórias, obras e representatividade em um só lugar.</p>
-            <button className='btn-show-artist'>Explorar Agora</button>
+            <button
+                className='btn-show-artist'
+                onClick={() => nv(baseURL+"artists")}
+            >Explorar Agora</button>
         </section>
     )
 }
